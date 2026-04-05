4bf5f861f5b593c881fcbbb86dd236a0594dbaaa
rihazs <rihazs@gmail.com>
2026-04-05 19:53:34 +0530

    Add roadmap folders

---
+++ skillbuild-system/a.sh
@@ -0,0 +1,20 @@
+touch 01_Core_Understanding/.gitkeep \
+02_Hardware/.gitkeep \
+03_Networking/.gitkeep \
+04_Operating_System/.gitkeep \
+05_FullStack_Foundation/.gitkeep \
+06_Angular/.gitkeep \
+07_Python_Backend/.gitkeep \
+08_Java_Backend/.gitkeep \
+09_Database/.gitkeep \
+10_Version_Control/.gitkeep \
+11_DevOps/.gitkeep \
+12_Cloud/.gitkeep \
+13_Testing/.gitkeep \
+14_Security/.gitkeep \
+15_Behavioural/.gitkeep \
+16_Learning_System/.gitkeep
+
+git add .
+git commit -m "Add roadmap folders"
+git push origin main
